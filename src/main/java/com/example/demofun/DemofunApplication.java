111b83346257e30f734efbdb036ef6ee4ad83fb1
darbyredhat <cdarby@redhat.com>
2022-06-06 15:32:38 -0400

    update

---
+++ src/main/java/com/example/demofun/DemofunApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofunApplication.class, args);
+	}
+
+}
