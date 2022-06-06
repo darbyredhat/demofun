a9f3ab20ed8ea2b76984c14cc1175ef61fad5291
darbyredhat <cdarby@redhat.com>
2022-06-06 16:02:45 -0400

    update

--- src/main/java/com/example/demofun/DemoController.java
+++ src/main/java/com/example/demofun/DemoController.java
@@ -1,13 +1,18 @@
 package com.example.demofun;
 
+import java.util.logging.Logger;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     
+    private Logger logger = Logger.getLogger(getClass().getName());
+
     @GetMapping("/hello")
     public String sayHello() {
+        logger.info(">>>>> inside Hello Yo!!!");
         return "Hello world! " + new java.util.Date();
     }
 }
