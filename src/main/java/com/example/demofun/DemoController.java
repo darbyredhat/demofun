3b68ae02571b730c3451e03dea8e31f58ea71f58
darbyredhat <cdarby@redhat.com>
2022-06-06 16:43:02 -0400

    update

--- src/main/java/com/example/demofun/DemoController.java
+++ src/main/java/com/example/demofun/DemoController.java
@@ -2,6 +2,7 @@ package com.example.demofun;
 
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,15 @@ public class DemoController {
     
     private Logger logger = Logger.getLogger(getClass().getName());
 
+    @Value("fav.city")
+    private String favCity;
+
+    @Value("fav.sport")
+    private String favSport;
+    
     @GetMapping("/hello")
     public String sayHello() {
         logger.info(">>>>> inside Hello Yo!!!");
-        return "Hello world! " + new java.util.Date();
+        return "Hello world! Fav Sport: " + favSport + ", Fav City: " + favCity;
     }
 }
